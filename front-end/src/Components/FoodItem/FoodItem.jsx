33e07d7407ea3978f007ae16816d330b51311100
Partha Mandal <parthamandal621@gmail.com>
2024-05-11 11:04:57 +0530

    Development till 11:04 am on 11-05-2024

--- front-end/src/Components/FoodItem/FoodItem.jsx
+++ front-end/src/Components/FoodItem/FoodItem.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
+import { frontend_assets } from '../../assets/frontend_assets/assets'
 
 const FoodItem = ({name,price,id,image,description}) => {
+
+  const [itemCount,setItemCount]=useState(0)
+
   return (
-    <div>
-      
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img src={image} alt="" className="food-item-image" />
+        {
+          !itemCount
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={frontend_assets.rating_starts}/>
+        </div>
+        <p className="food-item-description">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
   )
 }
